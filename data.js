f0d370986039725dbeee258a14e154c39bf946ef
E-RKP <edobackup111@gmail.com>
2025-05-28 16:16:45 +0200

    version 2

---
+++ data.js
@@ -0,0 +1,42 @@
+let store = {
+  prodotti: [
+
+    {
+      nome: 'NVIDIA MASTER',
+      quantity: 10,
+      price: 1049
+    },
+
+    {
+      nome: 'PC HAUQ',
+      quantity: 2,
+      price: 899
+    },
+
+    {
+      nome: 'EGGS',
+      quantity: 145,
+      price: 3.99
+    },
+
+    {
+      nome: 'HISENSE',
+      quantity: 11,
+      price: 599.90
+    },
+
+    {
+      nome: 'OP09',
+      quantity: 15,
+      price: 1049
+    },
+
+    {
+      nome: 'NVIDIA MASTER',
+      quantity: 10,
+      price: 1049
+    }
+
+
+  ]
+}
